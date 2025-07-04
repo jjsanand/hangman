91596063fd58049ad3042368fab0cf54929368b3
jagat <jagatdeep.anand@gmail.com>
2025-07-03 17:06:50 -0700

    refactored code

--- hangman.py
+++ hangman.py
@@ -3,18 +3,36 @@ import random
 word_list = ["aardvark"," buffalo", "cat", "dog"]
 chosenWord = random.choice(word_list)
 print(chosenWord)
-guess = input("Guess a letter ").lower()
 
 placeHolder = ""
 wordLen = len(chosenWord)
 for position in range(wordLen):
     placeHolder += "_"
+print(placeHolder)
 
-display = ""
-for letter in chosenWord:
-    if(letter == guess):
-        display += letter
-    else:
-        display += "_"
+
+
+game_over = False
+correct_letters=[]
+
+
+while not game_over:
+    guess = input("Guess a letter ").lower()
+    display = ""
+
+    for letter in chosenWord:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win")
+
+
 
 
